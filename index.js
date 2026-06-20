3e0a067ac8b60ef363acfd09a6c3de2cec681ccb
maximgrishin <>
2026-06-21 02:25:39 +0300

    Handle resizing

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 const PICO8_COLORS = [
-	'0 0 0',       // black
-	'29 43 83',    // dark_blue
-	'126 37 83',   // dark_purple
-	'0 135 81',    // dark_green
-	'171 82 54',   // brown
-	'95 87 79',    // dark_gray
-	'194 195 199', // light_gray
-	'255 241 232', // white
-	'255 0 77',    // red
-	'255 163 0',   // orange
-	'255 236 39',  // yellow
-	'0 228 54',    // green
-	'41 173 255',  // blue
-	'131 118 156', // indigo
-	'255 119 168', // pink
-	'255 204 170', // peach
+	'#000000', // black
+	'#1d2b53', // dark_blue
+	'#7e2553', // dark_purple
+	'#008751', // dark_green
+	'#ab5236', // brown
+	'#5f574f', // dark_gray
+	'#c2c3c7', // light_gray
+	'#fff1e8', // white
+	'#ff004d', // red
+	'#ffa300', // orange
+	'#ffec27', // yellow
+	'#00e436', // green
+	'#29adff', // blue
+	'#83769c', // indigo
+	'#ff77a8', // pink
+	'#ffccaa', // peach
 ];
 
-const canvas = document.createElement("canvas");
-const context = canvas.getContext("2d");
-context.font = "100px monospace";
-const metrics = context.measureText("1".repeat(33));
-let font_height = 100.*window.screen.width/metrics.width;
-if (window.screen.width > window.screen.height) {
-	font_height *= 0.75*window.screen.height/window.screen.width;
-}
-const font_width = (font_height/100)*metrics.width/33;
+const HEIGHT = 16;
+const WIDTH = 32;
 
-document.body.style = "border:0;margin:0;font-size:"+font_height+"px;font-family:monospace;white-space:nowrap;";
-document.body.style.background ="rgb("+PICO8_COLORS[0]+")"
-document.body.style.color = "rgb("+PICO8_COLORS[6]+")"
+document.body.style.background = PICO8_COLORS[0]
+document.body.style.color = PICO8_COLORS[6]
+document.body.style.margin = 0;
+document.body.style.padding = 0;
+document.body.style.fontFamily = "monospace";
+document.body.style.whiteSpace = "nowrap";
 
-let box = document.createElement("div");
-let cells = Array();
-for (let i = 0; i < 16; ++i) {
-	let row = Array();
-	for (let j = 0; j < 32; ++j) {
+const box = document.createElement("div");
+const cells = Array();
+for (let i = 0; i < HEIGHT; ++i) {
+	const row = Array();
+	for (let j = 0; j < WIDTH; ++j) {
 		const cell = document.createElement("span");
 		row.push(cell);
 		box.appendChild(cell);
@@ -43,19 +39,37 @@ for (let i = 0; i < 16; ++i) {
 	cells.push(row);
 	box.appendChild(document.createElement("br"));
 }
-
-const left_offset = (window.screen.width-font_width*33)/2;
 document.body.appendChild(box);
-box.style="border:solid "+font_width/2+"px;border-color:rgb("+PICO8_COLORS[5]+");width:fit-content;white-space:nowrap;position:absolute;left:"+left_offset+"px;top:0;";
-const help = document.createElement("p");
-help.style = "position:absolute;top:"+font_height*20+"px;left:"+left_offset+"px;";
-document.body.appendChild(help);
+box.style.width = "fit-content";
+box.style.margin = "auto";
+
+const fontSizeFor1pxScreenWidth = function() {
+	const canvas = document.createElement("canvas");
+	const context = canvas.getContext("2d");
+	const metricsHeight = 1000;
+	context.font = metricsHeight + "px monospace";
+	const metrics = context.measureText("a".repeat(WIDTH));
+	return metricsHeight/metrics.width;
+}();
+
+window.onresize = function() {
+	const currentFontSize = parseFloat(window.getComputedStyle(box, null).getPropertyValue('font-size'));
+	const landscapeFontSize = currentFontSize*window.innerHeight/box.offsetHeight;
+	const portraitFontSize = window.innerWidth*fontSizeFor1pxScreenWidth;
+	document.body.style.fontSize = Math.min(landscapeFontSize, portraitFontSize);
+};
+window.onresize();
+
 const inp = document.createElement("input");
-inp.style = "position:absolute;top:-1000px;left:"+left_offset+"px";
-document.body.appendChild(inp);
+inp.style.position = "relative";
+inp.style.top = -1000;
+document.addEventListener("click", () => {
+	inp.focus();
+});
+box.appendChild(inp);
 
 function cset(y, x, c) {
-	if (y < 0 || 16 <= y || x < 0 || 32 <= x) {
+	if (y < 0 || HEIGHT <= y || x < 0 || WIDTH <= x) {
 		return;
 	}
 	if (typeof(c) == typeof(0)) {
@@ -64,37 +78,35 @@ function cset(y, x, c) {
 	if (c == ' ') {
 		c = '\u00A0';
 	}
-	let bg = 0;
-	let fg = 7;
 	cells[y][x].textContent = c;
 }
 window.cset = cset;
 
 function bset(y, x, bg) {
-	if (y < 0 || 16 <= y || x < 0 || 32 <= x) {
+	if (y < 0 || HEIGHT <= y || x < 0 || WIDTH <= x) {
 		return;
 	}
-	if (0 <= bg && bg < 16) {
-		cells[y][x].style.background = 'rgb(' + PICO8_COLORS[bg] + ')'
+	if (0 <= bg && bg < PICO8_COLORS.length) {
+		cells[y][x].style.background = PICO8_COLORS[bg]
 	}
 }
 window.bset = bset;
 function fset(y, x, fg) {
-	if (y < 0 || 16 <= y || x < 0 || 32 <= x) {
+	if (y < 0 || HEIGHT <= y || x < 0 || WIDTH <= x) {
 		return;
 	}
-	if (0 <= fg && fg < 16) {
-		cells[y][x].style.color = 'rgb(' + PICO8_COLORS[fg] + ')'
+	if (0 <= fg && fg < PICO8_COLORS.length) {
+		cells[y][x].style.color = PICO8_COLORS[fg]
 	}
 }
 window.fset = fset;
 
 function cls() {
-	for (let i = 0; i < 16; ++i) {
-		for (let j = 0; j < 32; ++j) {
-			cset(i, j, ' ');
-			fset(i, j, 7);
-			bset(i, j, 0);
+	for (let y = 0; y < HEIGHT; ++y) {
+		for (let x = 0; x < WIDTH; ++x) {
+			cset(y, x, ' ');
+			fset(y, x, 7);
+			bset(y, x, 0);
 		}
 	}
 }
@@ -124,9 +136,11 @@ function print(y, x, msg) {
 	let i = 0;
 	for (c of msg) {
 		cset(y, x + i, c);
+		fset(y, x + i, 6);
 		++i;
 	}
 }
+print(14, 6, "controls: WASD, O, P");
 
 function btnp() {
 	if (inp.value != '') {
@@ -149,44 +163,17 @@ function btnp() {
 }
 window.btnp = btnp;
 
-let runtime_initialized = false;
-window.started = false;
-
-document.addEventListener("click", () => {
-	if (!runtime_initialized) {
-		print(13,2,"failed to load C++ module :(");
-		print(14,2,"try refreshing the page");
-	}
-	if (window.started == false) {
-		cls();
-		frameCallback();
-		help.textContent += 'controls: WASD, O, P';
-		if (audioCtx.state === "suspended") {
-			audioCtx.resume();
-		}
-		for (let i = 0; i < 4; ++i) {
-			osc[i].start(audioCtx.currentTime);
-		}
-		window.started = true;
-	}
-	inp.focus();
-});
-
 function frameCallback() {
 	window.update();
 	window.flip();
 	requestAnimationFrame(frameCallback);
 }
 
-function log(m) {
-	help.textContent += m;
-}
-
 const audioCtx = new AudioContext();
 const types = ["sine", "square", "triangle", "sawtooth"];
 const shares = [3/8, 1/8, 3/8, 1/8];
-let osc = [];
-let gain = [];
+const osc = [];
+const gain = [];
 for (let i = 0; i < 4; ++i) {
 	osc.push(new OscillatorNode(audioCtx, {
 	type: types[i],
@@ -219,5 +206,14 @@ const importObject = {env:{cset,bset,fset,btnp,nset,vset}};
 WebAssembly.instantiateStreaming(fetch("./compiled.wasm"), importObject).then((obj) => {
 	window.update = obj.instance.exports.call_update;
 	window.flip = obj.instance.exports.flip;
-	runtime_initialized = true;
+	box.addEventListener("click", () => {
+		cls();
+		frameCallback();
+		if (audioCtx.state === "suspended") {
+			audioCtx.resume();
+		}
+		for (let i = 0; i < 4; ++i) {
+			osc[i].start(audioCtx.currentTime);
+		}
+	}, {once: true});
 });
